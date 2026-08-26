38a99ebd4b8f831408d4bea9b500a6468a7a0010
Viral1976 <viralup@gmail.com>
2026-08-26 13:06:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function trackWhatsAppClick() {
         event_label: 'WhatsApp Button'
     });
 }
+function sendInquiryWhatsApp() {
+
+    const name = document.getElementById("inquiryName").value;
+    const mobile = document.getElementById("inquiryMobile").value;
+    const type = document.getElementById("insuranceType").value;
+    const message = document.getElementById("inquiryMessage").value;
+
+    const whatsappNumber = "918511169616";
+
+    const whatsappMessage =
+        "New Insurance Inquiry%0A%0A" +
+        "Name: " + encodeURIComponent(name) + "%0A" +
+        "Mobile: " + encodeURIComponent(mobile) + "%0A" +
+        "Insurance Type: " + encodeURIComponent(type) + "%0A" +
+        "Requirement: " + encodeURIComponent(message);
+
+    const whatsappURL =
+        "https://wa.me/" + whatsappNumber +
+        "?text=" + whatsappMessage;
+
+    window.open(whatsappURL, "_blank");
+}
